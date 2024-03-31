bfca46b5d616ca0a93938ba8f9410b8642974212
Jacob Zhou <zhoujaco9220@gmail.com>
2024-03-31 19:53:42 -0400

    Express Js Setup

---
+++ server/server.js
@@ -0,0 +1,16 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+io.on('connection', (socket) => {
+    console.log('User Online');
+
+    socket.on('canvas-data', (data) =>{
+        io.emit('canvas-data', data);
+    })
+})
+
+var server_port = 4000;
+http.listen(server_port, () => {
+    console.log("Started on : " + server_port);
+})
--- ui/kamehamedraw/src/components/board/Board.jsx
+++ ui/kamehamedraw/src/components/board/Board.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import io from 'socket.io-client';
+
 import './style.css';
 
 class Board extends React.Component {
 
     timeout;
+    socket = io.connect("http://localhost:3000");
 
     constructor(props) {
         super(props);
+
+        this.socket.on("canvas-data", function(data){
+            var image = new Image();
+            var canvas = document.querySelector('#board');
+            var context = canvas.getContext('2d');
+            image.onload = function(){
+                context.drawImage(image, 0, 0);
+            };
+            image.src = data;
+        })
     }
 
     componentDidMount() {
@@ -15,15 +28,15 @@ class Board extends React.Component {
 
     drawOnCanvas() {
         // Accessing 'this' context
-        const board = document.querySelector("#board");
-        const context = board.getContext("2d");
+        const canvas = document.querySelector("#board");
+        const context = canvas.getContext("2d");
 
         var sketch = document.querySelector('#sketch');
         var sketch_style = getComputedStyle(sketch);
 
         // Resizing
-        board.width = parseInt(sketch_style.getPropertyValue('width'));
-        board.height = parseInt(sketch_style.getPropertyValue('height'));
+        canvas.width = parseInt(sketch_style.getPropertyValue('width'));
+        canvas.height = parseInt(sketch_style.getPropertyValue('height'));
 
         // Variables
         var mouse = {x: 0, y: 0};
@@ -31,7 +44,7 @@ class Board extends React.Component {
 
         /* Mouse Capturing Work */
 
-        board.addEventListener('mousemove', function(e){
+        canvas.addEventListener('mousemove', function(e){
             last_mouse.x = mouse.x;
             last_mouse.y = mouse.y;
 
@@ -45,12 +58,12 @@ class Board extends React.Component {
         context.lineCap = 'round';
         context.strokeStyle = 'black';
 
-        board.addEventListener('mousedown', function(e){
-            board.addEventListener('mousemove', onPaint, false);
+        canvas.addEventListener('mousedown', function(e){
+            canvas.addEventListener('mousemove', onPaint, false);
         }, false);
 
-        board.addEventListener('mouseup', function(){
-            board.removeEventListener('mousemove', onPaint, false);
+        canvas.addEventListener('mouseup', function(){
+            canvas.removeEventListener('mousemove', onPaint, false);
         }, false);
 
         var root = this;
@@ -66,14 +79,15 @@ class Board extends React.Component {
                 clearTimeout(root.timeout);
             }
             root.timeout = setTimeout(function(){
-                var base64ImageData = board.toDataUrl("image/png");
+                var base64ImageData = canvas.toDataURL("image/png");
+                root.socket.emit("canvas-data", base64ImageData);
             }, 1000);
         }
     }
 
     render() {
         return (
-            <div class="sketch" id ="sketch">
+            <div className="sketch" id="sketch">
                 <canvas className="board" id="board"></canvas>
             </div>
         );
